e18498b67184b20970a54962758309908bc6edab
KshitijVerma2604 <76555272+KshitijVerma2604@users.noreply.github.com>
2022-03-03 21:28:21 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Name As String
+        Dim Marks As Integer
+
+        Name = TextBox1.Text
+        Marks = Val(TextBox2.Text)
+
+        If Marks > 85 And Marks <= 100 Then
+            MsgBox("Congratulations You Have Scored Grade A")
+        ElseIf Marks > 65 And Marks <= 85 Then
+            MsgBox("Congratulations You Have Scored Grade B")
+        ElseIf Marks > 50 And Marks <= 65 Then
+            MsgBox("Congratulations You Have Scored Grade C")
+        ElseIf Marks > 33 And Marks <= 50 Then
+            MsgBox("You Have Passed")
+        Else
+            MsgBox("You Have Failed")
+        End If
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
